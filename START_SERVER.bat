b74abc998d46bec62f0e69eb33613498dbec0545
Shambhoolal Narwaria <98864612+shambhoo-narwaria@users.noreply.github.com>
2026-02-24 00:23:35 +0530

    Delete START_SERVER.bat

--- START_SERVER.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Base64 Converter Local Server...
-echo.
-echo Server will start at: http://localhost:8000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-python -m http.server 8000
